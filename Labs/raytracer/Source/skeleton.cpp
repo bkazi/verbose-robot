cbd66e50e61a18250df305b64f5f5e599a6ed989
Bilal Kazi <bilalahmedkazi@hotmail.com>
2018-02-03 18:17:54 +0000

    Add spheres and try indirect specular
    Indirect specular is hard :(

--- Labs/raytracer/Source/skeleton.cpp
+++ Labs/raytracer/Source/skeleton.cpp
@@ -28,8 +28,7 @@ float m = numeric_limits<float>::max();
 vec4 lightPos(0, -0.5, -0.7, 1.0);
 vec3 lightColor = 14.f * vec3(1, 1, 1); 
 vec3 indirectLighting = 0.5f * vec3(1, 1, 1);
-float shininess = 50;
-float specularity = 0.1;
+float apertureSize = 0.1;
 
 /* ----------------------------------------------------------------------------*/
 /* STRUCTS                                                                     */
@@ -44,6 +43,7 @@ struct Camera {
 };
 
 struct Intersection {
+  bool isSphere;
   vec4 position;
   float distance;
   int triangleIndex;
@@ -53,20 +53,21 @@ struct Intersection {
 /* FUNCTIONS                                                                   */
 
 void Update(Camera &camera);
-void Draw(screen *screen, Camera camera, vector<Triangle> scene);
-bool ClosestIntersection(vec4 start, vec4 dir, vector<Triangle> &triangles, Intersection &closestIntersection);
-vec3 DirectLight(const Intersection &intersection, vec4 dir, vector<Triangle> &scene, bool spec);
-vec3 IndirectLight(const Intersection &intersection, vec4 dir, vector<Triangle> &scene, int bounce, bool spec);
+void Draw(screen *screen, Camera camera);
+bool ClosestIntersection(vec4 start, vec4 dir, Intersection &closestIntersection);
+vec3 DirectLight(const Intersection &intersection, vec4 dir, bool spec);
+vec3 IndirectLight(const Intersection &intersection, vec4 dir, int bounce, bool spec);
 mat3 CalcRotationMatrix(float x, float y, float z);
 vec3 uniformSampleHemisphere(const float &r1, const float &r2);
 void createCoordinateSystem(const vec3 &N, vec3 &Nt, vec3 &Nb);
 
+vector<Triangle> triangles;
+vector<Sphere> spheres;
 int main(int argc, char *argv[]) {
 
   screen *screen = InitializeSDL(SCREEN_WIDTH, SCREEN_HEIGHT, FULLSCREEN_MODE);
 
-  vector<Triangle> scene;
-  LoadTestModel(scene);
+  LoadTestModel(triangles, spheres);
 
   Camera camera = {
     SCREEN_HEIGHT,
@@ -81,8 +82,9 @@ int main(int argc, char *argv[]) {
   srand(42);
   // while (NoQuitMessageSDL()) {
     Update(camera);
-    Draw(screen, camera, scene);
+    Draw(screen, camera);
     SDL_Renderframe(screen);
+    Update(camera);
   // }
 
   SDL_SaveImage(screen, "screenshot.bmp");
@@ -92,7 +94,7 @@ int main(int argc, char *argv[]) {
 }
 
 /*Place your drawing here*/
-void Draw(screen *screen, Camera camera, vector<Triangle> scene) {
+void Draw(screen *screen, Camera camera) {
   /* Clear buffer */
   memset(screen->buffer, 0, screen->height * screen->width * sizeof(uint32_t));
 
@@ -108,22 +110,29 @@ void Draw(screen *screen, Camera camera, vector<Triangle> scene) {
       vec3 color = vec3(0);
       tempDepth = 0;
       if (NUM_RAYS <= 1) {
-        vec4 direction = glm::normalize(vec4(vec3((float) x, (float) y, camera.focalLength) * camera.R, 1));
-        if (ClosestIntersection(camera.position, direction, scene, intersection)) {
-          color += (DirectLight(intersection, direction, scene, true) + specularity * IndirectLight(intersection, direction, scene, BOUNCES, true) + (1 - specularity) * IndirectLight(intersection, direction, scene, BOUNCES, false)) * scene[intersection.triangleIndex].color;
+        vec4 direction = glm::normalize(vec4(vec3(x, y, camera.focalLength) * camera.R, 1));
+        if (ClosestIntersection(camera.position, direction, intersection)) {
+          vec3 tint = intersection.isSphere ? spheres[intersection.triangleIndex].color : triangles[intersection.triangleIndex].color;
+          float Ks = intersection.isSphere ? spheres[intersection.triangleIndex].Ks : triangles[intersection.triangleIndex].Ks;
+          float Kd = intersection.isSphere ? spheres[intersection.triangleIndex].Kd : triangles[intersection.triangleIndex].Kd;
+          color += (Kd * DirectLight(intersection, direction, false) + IndirectLight(intersection, direction, BOUNCES, false)) * tint + Ks * DirectLight(intersection, direction, true);
           tempDepth += intersection.distance;
         }
       } else {
         for (int i = -NUM_RAYS/2; i < NUM_RAYS/2; i++) {
-          float ep = (float) 1 / NUM_RAYS;
-          vec4 direction = glm::normalize(vec4(vec3((float) x + ep*i, (float) y + ep*i, camera.focalLength) * camera.R, 1));
-          if (ClosestIntersection(camera.position, direction, scene, intersection)) {
-            color += (DirectLight(intersection, direction, scene, true) + specularity * IndirectLight(intersection, direction, scene, BOUNCES, true) + (1 - specularity) * IndirectLight(intersection, direction, scene, BOUNCES, false)) * scene[intersection.triangleIndex].color;
-            tempDepth += intersection.distance;
+          for (int j = -NUM_RAYS/2; j < NUM_RAYS/2; j++) {
+            vec4 direction = glm::normalize(vec4(vec3((float) x + apertureSize*i, (float) y + apertureSize*j, camera.focalLength) * camera.R, 1));
+             if (ClosestIntersection(camera.position, direction, intersection)) {
+              vec3 tint = intersection.isSphere ? spheres[intersection.triangleIndex].color : triangles[intersection.triangleIndex].color;
+              float Ks = intersection.isSphere ? spheres[intersection.triangleIndex].Ks : triangles[intersection.triangleIndex].Ks;
+              float Kd = intersection.isSphere ? spheres[intersection.triangleIndex].Kd : triangles[intersection.triangleIndex].Kd;
+              color += (Kd * DirectLight(intersection, direction, false) + IndirectLight(intersection, direction, BOUNCES, false)) * tint + Ks * DirectLight(intersection, direction, true);
+              tempDepth += intersection.distance;
+            }
           }
         }
-        color /= NUM_RAYS;
-        tempDepth /= NUM_RAYS;
+        color /= NUM_RAYS * NUM_RAYS;
+        tempDepth /= NUM_RAYS * NUM_RAYS;
       }
       depth[x + SCREEN_WIDTH/2][y + SCREEN_HEIGHT/2] = tempDepth;
       if (tempDepth > maxDepth) {
@@ -206,18 +215,16 @@ void Update(Camera &camera) {
   camera.position += vec4(vec3(camera.movement * camera.R), 0);
 }
 
-bool ClosestIntersection(vec4 start, vec4 dir, vector<Triangle> &triangles, Intersection &closestIntersection) {
+bool ClosestIntersection(vec4 start, vec4 dir, Intersection &closestIntersection) {
   closestIntersection.distance = m;
   closestIntersection.triangleIndex = -1;
 
   for (uint index = 0; index < triangles.size(); index++) {
     Triangle triangle = triangles[index];
     vec4 v0 = triangle.v0;
-    vec4 v1 = triangle.v1;
-    vec4 v2 = triangle.v2;
-    vec3 e1 = vec3(v1) - vec3(v0);
-    vec3 e2 = vec3(v2) - vec3(v0);
-    vec3 b = vec3(start) - vec3(v0);
+    vec3 e1 = triangle.e1;
+    vec3 e2 = triangle.e2;
+    vec3 b = vec3(start - v0);
     mat3 A(-vec3(dir), e1, e2);
     float detA = glm::determinant(A);
     float dist = glm::determinant(mat3(b, e1, e2)) / detA;
@@ -227,10 +234,36 @@ bool ClosestIntersection(vec4 start, vec4 dir, vector<Triangle> &triangles, Inte
       float u = glm::determinant(mat3(-vec3(dir), b, e2)) / detA;
       float v = glm::determinant(mat3(-vec3(dir), e1, b)) / detA;
       if (u >= 0 && v >= 0 && u + v <= 1) {
+        closestIntersection.isSphere = false;
         closestIntersection.distance = dist;
         closestIntersection.triangleIndex = index;
         closestIntersection.position = vec4(vec3(v0) + (mat3(vec3(0), e1, e2) * vec3(0, u, v)), 1);
+        // closestIntersection.position = start + dist * dir;
       }
+    }
+  }
+  for (uint index = 0; index < spheres.size(); index++) {
+    Sphere sphere = spheres[index];
+    vec4 c = sphere.c;
+    float r = sphere.radius;
+    vec4 sC = start - c;
+    float inSqrt =  powf(r, 2.0f) - (glm::dot(sC, sC) - powf(dot(dir, sC), 2.0f));
+    float dist, dist1, dist2;
+    if (inSqrt < 0) {
+      continue;
+    }
+    else if (inSqrt == 0) {
+      dist = -dot(dir, sC);
+    } else {
+      dist1 = -dot(dir, sC) + sqrt(inSqrt);
+      dist2 = -dot(dir, sC) - sqrt(inSqrt);
+      dist = dist1 > 0 && dist1 < dist2 ? dist1 : dist2;
+    }
+    if (dist > 0 && dist < closestIntersection.distance) {
+      closestIntersection.isSphere = true;
+      closestIntersection.distance = dist;
+      closestIntersection.triangleIndex = index;
+      closestIntersection.position = start + dist * dir;
     }
   }
 
@@ -240,60 +273,72 @@ bool ClosestIntersection(vec4 start, vec4 dir, vector<Triangle> &triangles, Inte
 std::default_random_engine generator; 
 std::uniform_real_distribution<float> distribution(0, 1);
 
-vec3 IndirectLight(const Intersection &intersection, vec4 dir, vector<Triangle> &scene, int bounce, bool spec) {
+vec3 IndirectLight(const Intersection &intersection, vec4 dir, int bounce, bool spec) {
   if (BOUNCES == 0) {
     return indirectLighting;
   }
   if (bounce == 0) {
-    return vec3(0);
+    return DirectLight(intersection, dir, false);
   } else {
-    vec4 n = scene[intersection.triangleIndex].normal;
-    if (spec) {
-      Intersection reflectIntersection;
-      vec4 reflect = glm::normalize(glm::reflect(dir, n));
-      if (ClosestIntersection(intersection.position + (reflect * 1e-4f), reflect, scene, reflectIntersection)) {
-        vec3 P = (DirectLight(reflectIntersection, reflect, scene, false) + IndirectLight(reflectIntersection, reflect, scene, bounce - 1, true)) * scene[reflectIntersection.triangleIndex].color;
-        return (P * max(glm::dot(reflect, n), 0.0f));
-      }
+    vec4 n;
+    if (intersection.isSphere) {
+      n = glm::normalize(intersection.position - spheres[intersection.triangleIndex].c);
     } else {
-      vec3 light = vec3(0);
-      vec3 Nt, Nb;
-      createCoordinateSystem(vec3(n), Nt, Nb);
-      for (int i = 0; i < NUM_SAMPLES; i++) {
-        Intersection reflectIntersection;
-        float r1 = distribution(generator);
-        float r2 = distribution(generator);
-        vec3 sample = uniformSampleHemisphere(r1, r2); 
-        vec3 sampleWorld(mat3(Nb, vec3(n), Nt) * sample);
+      n = triangles[intersection.triangleIndex].normal;
+    }
+    vec3 light = vec3(0);
+    vec3 Nt, Nb;
+    createCoordinateSystem(vec3(n), Nt, Nb);
+    int count = 0;
+    for (int i = 0; i < NUM_SAMPLES; i++) {
+      if (rand() / RAND_MAX > 0.5) continue;
+      else count++;
+      Intersection reflectIntersection;
+      float r1 = distribution(generator);
+      float r2 = distribution(generator);
+      vec3 sample = uniformSampleHemisphere(r1, r2); 
+      vec3 sampleWorld;
+      if (spec) {
+        sampleWorld = vec3(mat3(Nb, vec3(glm::reflect(dir, n)), Nt) * sample);
+        vec3 P = IndirectLight(reflectIntersection, vec4(sampleWorld, 1), bounce - 1, spec);
+        light += (P * max(glm::dot(vec4(sampleWorld, 1), dir), 0.0f));
+      } else {
+        sampleWorld = vec3(mat3(Nb, vec3(n), Nt) * sample);
         vec4 rayDir = vec4(sampleWorld, 1);
-        if (ClosestIntersection(intersection.position + (rayDir * 1e-4f), rayDir, scene, reflectIntersection)) {
-          vec3 P = (DirectLight(reflectIntersection, rayDir, scene, false) + IndirectLight(reflectIntersection, rayDir, scene, bounce - 1, false)) * scene[reflectIntersection.triangleIndex].color;
-          light += (P * max(glm::dot(rayDir, n), 0.0f));
+        if (ClosestIntersection(intersection.position + (rayDir * 1e-4f), rayDir, reflectIntersection)) {
+          vec3 tint = reflectIntersection.isSphere ? spheres[reflectIntersection.triangleIndex].color : triangles[reflectIntersection.triangleIndex].color;
+          vec3 P = IndirectLight(reflectIntersection, rayDir, bounce - 1, spec);
+          light += (P * max(glm::dot(rayDir, n), 0.0f)) * tint;
         }
       }
-      light /= NUM_SAMPLES;
-      return light;
     }
-    return vec3(0);
+    light /= count;;
+    return light;
   }
 }
 
-vec3 DirectLight(const Intersection &intersection, vec4 dir, vector<Triangle> &scene, bool spec) {
+vec3 DirectLight(const Intersection &intersection, vec4 dir, bool spec) {
   vec3 P = lightColor;
-  vec4 n = scene[intersection.triangleIndex].normal;
+  vec4 n;
+  if (intersection.isSphere) {
+    n = glm::normalize(intersection.position - spheres[intersection.triangleIndex].c);
+  } else {
+    n = triangles[intersection.triangleIndex].normal;
+  }
   vec4 r = lightPos - intersection.position;
   float rL = glm::length(r);
   vec4 rN = r / rL;
   Intersection shadowIntersection;
-  if (ClosestIntersection(intersection.position + (n * 1e-4f), rN, scene, shadowIntersection)) {
+  if (ClosestIntersection(intersection.position + (rN * 1e-4f), rN, shadowIntersection)) {
     float distance = glm::distance(intersection.position, shadowIntersection.position);
     if (distance < rL) {
       return vec3(0);
     }
   }
-  vec4 reflected = glm::reflect(dir, n);
   if (spec) {
-    return (P * (max(glm::dot(rN, n), 0.0f) + max(powf(glm::dot(reflected, rN), shininess), 0.0f))) / (float) (4 * M_PI * powf(rL, 2));
+    vec4 reflected = glm::reflect(rN, n);
+    float shininess = intersection.isSphere ? spheres[intersection.triangleIndex].shininess : triangles[intersection.triangleIndex].shininess;
+    return (P * max(powf(glm::dot(reflected, dir), shininess), 0.0f)) / (float) (4 * M_PI * powf(rL, 2));
   } else {
     return (P * max(glm::dot(rN, n), 0.0f)) / (float) (4 * M_PI * powf(rL, 2));
   }
